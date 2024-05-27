2cef0d33ba69f02262675cd8cfd27500b99de908
SUHAIL786553 <ms5505535@gmail.com>
2024-05-27 10:25:25 +0530

    Updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,55 +1,136 @@
-import React from 'react';
-import './Content.css'
+import React from "react";
+import "./Content.css";
 // Define a functional component
 function Content() {
   return (
     <div>
-        <div className="main">
-            <h1>Your New Gateway to<br></br> Exclusive Investment In <br></br>Best Opportunities</h1>
-        </div>
-        <div className="main1">
-          <p>Believe that knowledge is key to confident decesion making that's why we provide clients with the wealth of educational resources market insights,and timely updates.</p>
-        </div>
-        <div className="button-container">
+      <div className="main">
+        <h1>
+          Your New Gateway to<br></br> Exclusive Investment In <br></br>Best
+          Opportunities
+        </h1>
+      </div>
+      <div className="main1">
+        <p>
+          Believe that knowledge is key to confident decesion making that's why
+          we provide clients with the wealth of educational resources market
+          insights,and timely updates.
+        </p>
+      </div>
+      <div className="button-container">
         <button className="responsive-button">BIND WITH US</button>
         <button className="responsive-button1">CONTACT US</button>
-    </div>
-    <div className="container">
-        <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b8f585cd44c52b3679a41c_v1%20banner%20img-p-800.png" alt="Responsive Image" class="responsive-image"/>
-    </div>
-    <section className="write-content">
-    <div className="Content">
-            <h1>Trusted By 40K+ Brands<br></br>All Over The World</h1>
+      </div>
+      <div className="container">
+        <img
+          src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b8f585cd44c52b3679a41c_v1%20banner%20img-p-800.png"
+          alt="Responsive Image"
+          className="responsive-image"
+        />
+      </div>
+      <section className="write-content">
+        <div className="Content">
+          <h1>
+            Trusted By 40K+ Brands<br></br>All Over The World
+          </h1>
         </div>
         <div className="Content1">
-          <p>Believe that knowledge is key to confident decesion making that's why we provide clients with the wealth of educational resources market insights,and timely updates.</p>
+          <p>
+            Believe that knowledge is key to confident decesion making that's
+            why we provide clients with the wealth of educational resources
+            market insights,and timely updates.
+          </p>
         </div>
         {/* logos running */}
         <div class="logo-row">
-        <div class="logos">
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg" alt="Logo 1"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg" alt="Logo 2"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg" alt="Logo 3"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a0556340cfea41a9678_company%20logo%204.svg" alt="Logo 4"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b909f6c024b60878afb81e_company%20logo%203.svg" alt="Logo 4"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg" alt="Logo 7"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg" alt="Logo 8"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg" alt="Logo 8"/>
+          <div class="logos">
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg"
+              alt="Logo 1"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg"
+              alt="Logo 2"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg"
+              alt="Logo 3"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a0556340cfea41a9678_company%20logo%204.svg"
+              alt="Logo 4"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b909f6c024b60878afb81e_company%20logo%203.svg"
+              alt="Logo 4"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg"
+              alt="Logo 7"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg"
+              alt="Logo 8"
+            />
+          </div>
         </div>
-    </div>/
-    <div class="logo-row">
-        <div class="logos">
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a4e4b3f980c78aad7b5_company%20logo%2010.svg" alt="Logo 5"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a566d7044174d4a05c3_company%20logo%2011.svg" alt="Logo 6"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg" alt="Logo 7"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg" alt="Logo 8"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg" alt="Logo 8"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg" alt="Logo 1"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg" alt="Logo 2"/>
-            <img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg" alt="Logo 3"/>
+        /
+        <div class="logo-row">
+          <div class="logos">
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a4e4b3f980c78aad7b5_company%20logo%2010.svg"
+              alt="Logo 5"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a566d7044174d4a05c3_company%20logo%2011.svg"
+              alt="Logo 6"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg"
+              alt="Logo 7"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg"
+              alt="Logo 1"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg"
+              alt="Logo 2"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg"
+              alt="Logo 3"
+            />
+          </div>
+        </div>
+      </section>
+      
+      <div class="container1">
+        <div class="content2">
+        <h2>
+            <span className="span">Understand</span>that navigating the complex world of <span className="invest"> Investment </span>
+            requires a blend of expertise, insight, and a forward-thinking
+            approach. Our seasoned team of <span className="finance">Financial</span>
+          </h2>
+        </div>
+        <div class="photo-gallery">
+            <div class="photo" id="photo1"></div>
+            <div class="photo" id="photo2"></div>
+            <div class="photo" id="photo3"></div>
         </div>
     </div>
-    </section>
     </div>
   );
 }
