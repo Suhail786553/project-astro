828709fe74406792818c9419030758f134ab5820
SUHAIL786553 <ms5505535@gmail.com>
2024-06-07 14:06:49 +0530

    Add Netlify adapter for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
-// import { defineConfig } from 'astro/config';
-
-// import react from "@astrojs/react";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   integrations: [react()],
-//   redirects: {
-//     '/old-page': '/new-page'
-//   }
-// });
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import netlify from "@astrojs/netlify";
 export default defineConfig({
-  output: 'server', // Change to 'server' or 'hybrid'
+  output: 'server',
+  // Change to 'server' or 'hybrid'
   integrations: [react()],
+  adapter: netlify()
 });
