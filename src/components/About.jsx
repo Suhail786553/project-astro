9663ee921fe32262105349c5c2dab7824e2de700
SUHAIL786553 <ms5505535@gmail.com>
2024-06-08 13:24:26 +0530

    all

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './About.css'
+import InteractiveDivs from './InteractiveDivs';
 
 const About = () => {
   return (
@@ -26,6 +27,222 @@ const About = () => {
         </div>
     </div>
 </div>
+<div class="container-row1">
+        <div class="row-1">
+            <div class="photo1"><img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65bf76c5ce8ad963b4e81b72_about%20us%20img%201.png" alt="Photo 1"/></div>
+            <div class="photo1"><img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65bf76eb9cd3b97eab3d61c7_about%20us%20img%202.png" alt="Photo 2"/></div>
+        </div>
+        <div class="row-1">
+            <div class="photo1"><img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65bf775df4ca11d2994fb37a_about%20us%20img%203.png" alt="Photo 3"/></div>
+            <div class="photo1"><img src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65bf7769b68dbbea9edccd46_about%20us%20img%204.png" alt="Photo 4"/></div>
+        </div>
+    </div>
+    <div className="container-6">
+    <div className="bxx-1">
+        <h1>Explore About Zaivest, Why People Select It.</h1>
+    </div>
+    <div className="logo-container">
+        <img src="https://icon-library.com/images/rotation-icon/rotation-icon-17.jpg" alt="Zaivest Logo" className="rotating-logo"/>
+    </div>
+    <div className="bxx-2">
+        <p>
+            Stay informed, stay empowered – because when you understand your
+            investments, that you're future better equipped to achieve your
+            financial goals.
+        </p>
+    </div>
+</div>
+<InteractiveDivs/>
+<div class="container-content">
+        <h1>Innovators, Creators<br></br> And Digital Navigators.</h1>
+        <h4>Your trusted partner in the digital realm. We specialize in crafting <br></br> the product that look stunning & deliver seamless user experiences.</h4>
+    </div>
+    <div className="logo-row">
+          <div className="logos">
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg"
+              alt="Logo 1"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg"
+              alt="Logo 2"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg"
+              alt="Logo 3"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a0556340cfea41a9678_company%20logo%204.svg"
+              alt="Logo 4"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b909f6c024b60878afb81e_company%20logo%203.svg"
+              alt="Logo 4"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg"
+              alt="Logo 7"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg"
+              alt="Logo 8"
+            />
+          </div>
+        </div>
+        <div className="logo-row">
+          <div className="logos">
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a4e4b3f980c78aad7b5_company%20logo%2010.svg"
+              alt="Logo 5"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a566d7044174d4a05c3_company%20logo%2011.svg"
+              alt="Logo 6"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a6eb3e661b51a05277e_company%20logo%2012.svg"
+              alt="Logo 7"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a7a48f65e29f54cbb6c_company%20logo%2013.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a439d9c21c56b921fa1_company%20logo%209.svg"
+              alt="Logo 8"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2036b309cc89f56bfe_company%20logo%206.svg"
+              alt="Logo 1"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a11029aa8acd8eb622e_company%20logo%205.svg"
+              alt="Logo 2"
+            />
+            <img
+              src="https://assets-global.website-files.com/65b8d5c5e2ef493dbe7c1b86/65b90a2d33c28b61a374ab53_company%20logo%207.svg"
+              alt="Logo 3"
+            />
+          </div>
+        </div>
+        <div className="container1">
+        <div className="content2">
+          <h2>
+            <span className="span">Understand</span>that navigating the complex
+            world of <span className="invest"> Investment </span>
+            requires a blend of expertise, insight, and a forward-thinking
+            approach. Our seasoned team of{" "}
+            <span className="finance">Financial</span>
+          </h2>
+        </div>
+        <div className="photo-gallery">
+          <div className="photo" id="photo1"></div>
+          <div className="photo" id="photo2"></div>
+          <div className="photo" id="photo3"></div>
+        </div>
+      </div>
+      <div className="containers">
+        <div className="step-connector">
+          <div className="step">
+            Step 1 <h4>Discovery</h4>
+          </div>
+          <div className="connector"></div>
+          <div className="step">
+            Step 2<h4>Research</h4>
+          </div>
+          <div className="connector"></div>
+          <div className="step">
+            Step 3<h4>Planning</h4>
+          </div>
+          <div className="buttons-step">
+            <button classname="one-btn">BIND US</button>
+            <button>CONTACT US</button>
+          </div>
+        </div>
+        <div className="scroll-container">
+          <div className="image" id="image4"></div>
+          <div className="image" id="image3"></div>
+          <div className="image" id="image2"></div>
+          <div className="image" id="image1"></div>
+        </div>
+      </div>
+      <div className="container3">
+        <div className="box1">
+          <h1>Explore About Zaivest, Why People Select It.</h1>
+        </div>
+        <div className="box2">
+          <p>
+            Stay informed, stay empowered – because when you understand your
+            investments, that you're future better equipped to achieve your
+            financial goalsand achieve ny other goals like partenship bussiness
+            or any other type of Bussiness
+          </p>
+        </div>
+      </div>
+      <div />
+      <footer>
+        <div className="footer-logo">
+          <img
+            src="https://app.shiprocket.in/sr_login/assets/images/srlogo.svg
+"
+            alt="Logo"
+          />
+        </div>
+        <div className="footer-subscribe">
+          <div className="subscribe-content">
+            <p>Enter Your Email</p>
+            <button>SUBSCRIBE</button>
+          </div>
+        </div>
+        <div className="footer-info">
+          <div className="info-column">
+            <h4>Pages</h4>
+            <p>Home </p>
+            <p>About</p>
+            <p>About</p>
+            <p>Shop</p>
+            <p>Contact Us</p>
+          </div>
+          <div className="info-column">
+            <h4>Company Pages</h4>
+            <p>Shop Single</p>
+            <p>Blog Single</p>
+            <p>Blog</p>
+            <p>Project</p>
+            <p>Project Single</p>
+          </div>
+          <div className="info-column">
+            <h4>Template</h4>
+            <p>404 Not Found</p>
+            <p>Change Log</p>
+            <p>License</p>
+            <p>Style Guide</p>
+            <p>Password Protected</p>
+          </div>
+          <div className="info-column">
+            <h4>Contact Us</h4>
+            <p>
+              <i className="fab fa-facebook"></i> Facebook
+            </p>
+            <p>
+              <i className="fab fa-twitter"></i> Twitter
+            </p>
+            <p>
+              <i className="fab fa-instagram"></i> Instagram
+            </p>
+            <p>
+              <i className="fab fa-linkedin"></i> Linkedin
+            </p>
+            <p>
+              <i className="fab fa-skype"></i> Skype
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
